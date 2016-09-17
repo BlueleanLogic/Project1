9d5c96c6b9db12fd945e1e9d61ff1b52547d4880
Brittany Madrigal <bluemadrigal@gmail.com>
2016-09-17 14:31:59 -0500

    Just starting to add cameras

---
+++ appdirectory/TutorialApplication.cpp
@@ -0,0 +1,92 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#include "TutorialApplication.h"
+
+//---------------------------------------------------------------------------
+TutorialApplication::TutorialApplication(void)
+{
+}
+//---------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//---------------------------------------------------------------------------
+void TutorialApplication::createScene(void)
+{
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+}
+//---------------------------------------------------------------------------
+
+void TutorialApplication::createCamera()
+{
+    mCamera = mSceneMgr->createCamera("PlayerCam");
+    //position the camera
+    mCamera->setPosition(Ogre::Vector3(0, 300, 500));
+    //sets direction of camera
+    mCamera->lookAt(Ogre::Vector3(0, 0, 0));
+    //the distance at which the Camera will no longer render any mesh
+    mCamera->setNearClipDistance(5);
+    //camera controller, can control camera passed to it
+    mCameraMan = new OgreBites::SdkCameraMan(mCamera);
+}
+ 
+void TutorialApplication::createViewports()
+{
+    Ogre::Viewport* vp = mWindow->addViewport(mCamera);
+    //set the background color of the Viewport
+    vp->setBackgroundColour(Ogre::ColourValue(0, 0, 0));
+}
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        } catch(Ogre::Exception& e)  {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occurred: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+//---------------------------------------------------------------------------
